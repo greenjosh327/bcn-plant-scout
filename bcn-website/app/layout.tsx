f9c2835c7f5baf5d5f4eba1f19a4c4399062de41
BCN Plant Scout <greenjosh327@gmail.com>
2026-07-11 08:01:24 -0400

    Add Google Analytics tag

--- bcn-website/app/layout.tsx
+++ bcn-website/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Footer } from "@/components/footer";
 import { SiteHeader } from "@/components/site-header";
@@ -13,6 +14,19 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KHYPDHB4W4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KHYPDHB4W4');
+          `}
+        </Script>
         <SiteHeader />
         {children}
         <Footer />
